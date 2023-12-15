91dc5b28c1b150687a55a32e0fda9d47a5e2974b
mksdevops7 <manesorapalli@gmail.com>
2023-12-15 18:00:31 +0530

    from terminal

---
+++ mongodb.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+B="/e[1m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+if [ ID -nq 0 ]
+then 
+    echo "ERROR :: become root user to Execute the script"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $B $R FAILED $N $N"
+        exit 1
+    else
+        echo -e "$2 ... $B $G SUCCESS $N $N"
+    fi
+}
+
+set-hostname mongodb
+timedatectl set-timezone Asia/Kolkata
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting MongoDB"
