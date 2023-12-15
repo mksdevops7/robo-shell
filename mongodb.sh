35257fd93f08831baac0308e0bf8e5af7123f03c
mksdevops7 <manesorapalli@gmail.com>
2023-12-15 18:30:16 +0530

    from terminal

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 sudo timedatectl set-timezone Asia/Kolkata
 TIMESTAMP=$(date +%F-%H-%M-%S)
