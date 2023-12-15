73c2caa69d0f2bfe5ea05e842d714691dea20874
mksdevops7 <manesorapalli@gmail.com>
2023-12-15 20:17:58 +0530

    from terminal

--- mongodb.sh
+++ mongodb.sh
@@ -28,7 +28,7 @@ then
     echo -e "$B $R ERROR:: become root user to Execute the script $N $N"
     exit 1
 else
-    echo -e "$Y You are root user $N"
+    echo -e "$B $Y You are a root user $N $N"
 fi
 
 #sudo nmcli g hostname manee
