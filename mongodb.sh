29f6fbac17087c9caa9998789c59c50c502effd9
mksdevops7 <manesorapalli@gmail.com>
2023-12-15 18:16:38 +0530

    from terminal

--- mongodb.sh
+++ mongodb.sh
@@ -25,7 +25,7 @@ VALIDATE(){
 
 if [ $ID -ne 0 ]
 then 
-    echo "$B $R ERROR :: become root user to Execute the script $N"
+    echo "$R "/e[1mERROR :: become root user to Execute the script/e[0m" $N"
     exit 1
 else
     echo "You are root user"
