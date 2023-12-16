2e7054e052d59b24406790862cfa3f9a7ce2b87f
mksdevops7 <manesorapalli@gmail.com>
2023-12-16 20:46:25 +0530

    robo-shell script

--- payment.sh
+++ payment.sh
@@ -11,7 +11,7 @@ sudo timedatectl set-timezone Asia/Kolkata
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-TIMESTAMP.log"
 
-exec &>> $LOGFILE 
+# exec &>> $LOGFILE 
 # executes a Shell command without creating a new process.
 # instead of giving $LOGFILE every where we are giving here to avoid repetition
 
@@ -40,7 +40,7 @@ dnf install python36 gcc python3-devel -y &>> $LOGFILE
 id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
-    useradd roboshop
+    useradd roboshop &>> $LOGFILE
     VALIDATE $? "roboshop user creation"
 else
     echo -e "roboshop user already exist $Y SKIPPING $N"
