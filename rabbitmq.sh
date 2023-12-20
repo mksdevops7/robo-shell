0ee8b11d8bb62aa9c1421fd243e85ac5afed8f3f
mksdevops7 <manesorapalli@gmail.com>
2023-12-20 13:09:41 +0530

    robo-shell script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -50,7 +50,7 @@ VALIDATE $? "Enabling rabbitmq server"
 systemctl start rabbitmq-server  &>> $LOGFILE
 VALIDATE $? "Starting rabbitmq server"
 
-rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+rabbitmqctl -f add_user roboshop roboshop123 &>> $LOGFILE
 VALIDATE $? "creating user"
 
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
